0916861834b598b70775637b67f1754075d7be3c
Adulla Qassim <aboodyaq7@gmail.com>
2023-04-11 13:51:47 +0300

    Added Nav component

--- src/App.js
+++ src/App.js
@@ -10,10 +10,12 @@ import Website from './website/Website'
 import WebsiteCreate from './website/WebsiteCreate';
 import WebsiteCreateB from './buildWebsite/WebsiteCreate';
 import UserList from './admin/UserList';
+import Nav from './components/Nav';
 
-function App() {
+function App(props) {
   const [currentUser, setCurrentUser] = useState(null);
   const [isAdmin, setIsAdmin] = useState(null);
+  
 
 
   useEffect(() => {
@@ -57,100 +59,11 @@ function App() {
       });
   };
 
+ 
+
   return (
     <Router>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-        <div className="container-fluid">
-          <Link className="navbar-brand" to="/">
-            CMS App
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link className="nav-link" to="/">
-                  Home
-                </Link>
-              </li>
-              {currentUser ? (
-                <>
-                  <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle d-flex align-items-center" href="/#" role="button" id="userDropdown" data-bs-toggle="dropdown" aria-expanded="false">
-                      <img referrerPolicy='no-referrer' src={currentUser.avatarURL || "/guest.jpeg"} alt="Profile" className="rounded-circle me-2" style={{ width: 24, height: 24, objectFit: 'cover' }} />
-                      {currentUser.firstName}
-                    </a>
-                    <ul className="dropdown-menu" aria-labelledby="userDropdown">
-                      <li>
-                        <Link className="dropdown-item" to="/profile">
-                          Profile
-                        </Link>
-                      </li>
-                      <li>
-                        <button className="dropdown-item" onClick={signOut}>
-                          Sign Out
-                        </button>
-                      </li>
-
-                    </ul>
-                  </li>
-                 { isAdmin? ( 
-                    <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle d-flex align-items-center" href="/#" role="button" id="userDropdown" data-bs-toggle="dropdown" aria-expanded="false">
-                        Admin Portal
-                        </a>
-                        <ul className="dropdown-menu" aria-labelledby="userDropdown">
-                          <li>
-                            <Link className="dropdown-item" to="/users">
-                              Manage Users
-                            </Link>
-                          </li>
-                        </ul>
-                    </li>
-                  ): null}
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/website/WebDevGuru/">
-                      Website WebDevGuru
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/Create">
-                      Create website
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/Createb">
-                      Create websiteB
-                    </Link>
-                  </li>
-                </>
-              ) : (
-                <>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/login">
-                      Log In
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/signup">
-                      Sign Up
-                    </Link>
-                  </li>
-                </>
-              )}
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <Nav currentUser={currentUser} signOut={signOut} />
 
       <div className="container-fluid" style={{ paddingTop: "50px" }}>
         <div className="container py-4">
