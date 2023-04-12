73c0b6b424fec99e62a03f844db333ece9d4040c
anas029 <aliyaqoob+github@gmail.com>
2023-04-13 00:26:59 +0300

    Edit

--- src/buildWebsite/WebsiteEditor.js
+++ src/buildWebsite/WebsiteEditor.js
@@ -2,8 +2,9 @@ import Axios from "axios"
 import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import PageEditor from "./PageEditor"
+import DomainForm from "./DomainForm"
 
-export default function DomainForm(props) {
+export default function WebsiteEditor(props) {
 
     return (
         <>
